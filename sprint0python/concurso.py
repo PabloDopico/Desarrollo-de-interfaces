3338c814c00164045c4c842769acaf7cec77a8c1
Pablo Dopico <pdopicob@fpcoruna.afundacion.org>
2022-09-27 09:07:53 +0200

    Completado ejercicio 1

---
+++ sprint0python/concurso.py
@@ -0,0 +1,21 @@
+print ("¿Cuánto es la raíz cuadrada de 144?:")
+
+print ("a) 12")
+print ("b) 14")
+print ("c) 16")
+
+print()
+respuesta = input("Respuesta: ")
+print ()
+
+if respuesta==str(12):
+	print("¡Respuesta correcta!")
+else:
+	print("¡Respuesta incorrecta!")
+	
+
+
+
+
+
+
