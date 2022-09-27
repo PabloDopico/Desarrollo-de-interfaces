998400d6fb8754fd9a6dad31cba14490e49fcce6
Pablo Dopico <pdopicob@fpcoruna.afundacion.org>
2022-09-27 10:12:09 +0200

    Completado ejercicio 4

---
+++ sprint0python/fibonacci2.py
@@ -0,0 +1,6 @@
+from math import sqrt
+def funcion_fibonacci2(n):
+	   return ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
+
+
+
--- sprint0python/principal.py
+++ sprint0python/principal.py
@@ -1,8 +1,12 @@
 from fibonacci import funcion_fibonacci
+from fibonacci2 import funcion_fibonacci2
 
-print("Teclea número inicial:")
+print("Teclea número:")
 n = input()
+print (funcion_fibonacci(float(n)))
 
-print (funcion_fibonacci(int(n)))
+print("Teclea número: ")
+n2 = input()
+print (funcion_fibonacci2(float(n2)))
 
 
