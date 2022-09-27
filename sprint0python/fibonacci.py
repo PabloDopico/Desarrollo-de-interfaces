16ed1fe4ec69e071de9d37d6cb199b393a0093da
Pablo Dopico <pdopicob@fpcoruna.afundacion.org>
2022-09-27 09:41:12 +0200

    Completado ejercicio 3

---
+++ sprint0python/fibonacci.py
@@ -0,0 +1,7 @@
+def funcion_fibonacci(n):
+   if n == 0 or n == 1:
+       return n
+   else:
+       return(funcion_fibonacci(n-1) + funcion_fibonacci(n-2))
+
+
---
+++ sprint0python/principal.py
@@ -0,0 +1,8 @@
+from fibonacci import funcion_fibonacci
+
+print("Teclea número inicial:")
+n = input()
+
+print (funcion_fibonacci(int(n)))
+
+
