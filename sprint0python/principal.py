c671b6e092dc46d4962de4f25ecb351764567d5b
Pablo Dopico <pdopicob@fpcoruna.afundacion.org>
2022-09-28 09:27:17 +0200

    Completado ejercicio 6

--- sprint0python/principal.py
+++ sprint0python/principal.py
@@ -1,5 +1,6 @@
 from fibonacci import funcion_fibonacci
 from fibonacci2 import funcion_fibonacci2
+import time
 
 print()
 print("\t\tMENU\n")
@@ -12,13 +13,20 @@ while (opcion != 'a') & (opcion != 'b'):
     opcion = input("La opción debe ser a o b: ")
 print()
 
-if(opcion=="a"):
+if opcion == 'a':
     print("Teclea número:")
     n = input()
-    print (funcion_fibonacci(float(n)))
+    start = time.time()  # Comienza a contar
+    print(funcion_fibonacci(float(n)))
+    end = time.time()  # Termina de contar
+    elapsed = end - start
+    print('El tiempo de ejecución ha sido :' + str(elapsed) + ' s')
+
 else:
     print("Teclea número: ")
     n2 = input()
-    print (funcion_fibonacci2(float(n2)))
-
-
+    start = time.time()
+    print(funcion_fibonacci2(float(n2)))
+    end = time.time()
+    elapsed = end - start
+    print('El tiempo de ejecución ha sido :' + str(elapsed) + ' s')
