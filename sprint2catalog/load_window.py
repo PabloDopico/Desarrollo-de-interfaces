e44f7702b609b66ba6f1862589f7452d557fc8da
Pablo Dopico <pdopicob@fpcoruna.afundacion.org>
2022-10-19 10:43:42 +0200

    Completado ejercicio 3

--- sprint2catalog/load_window.py
+++ sprint2catalog/load_window.py
@@ -1,19 +1,49 @@
 import gi
+import requests,threading,shutil
 gi.require_version("Gtk","3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk, GLib
+
+from window import MainWindow
 
 class LoadWindow(Gtk.Window):
-    label = Gtk.Label("Cargando elementos...")
-    spinner = Gtk.Spinner()
-    box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=20)
+    label = Gtk.Label("Cargando elementos...")  ## declaramos una etiqueta
+    spinner = Gtk.Spinner() ## declaramos un componente spinner (círculo de carga)
+    box = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 20) ## declaramos una caja
 
     def __init__(self):
         super().__init__(title="")
         self.connect("destroy", Gtk.main_quit)
-        self.set_border_width(60)
-        self.set_resizable(False)
-        self.spinner.props.active=True
+        self.set_border_width(60) ## indicamos el borde
+        self.set_resizable(False) ## indicamos si la ventana sera reajustable
+        self.spinner.props.active = True ## activamos el spinner
 
-        self.box.pack_start(self.label, False, False, 0)
+        self.box.pack_start(self.label, False, False, 0) ## añadimos label y spinner a la caja
         self.box.pack_start(self.spinner, False, False, 0)
         self.add(self.box)
+        self.launch_load() ## iniciamos el metodo launch_load
+
+    def start_main_window(self,loaded_itemos_list):
+        win=MainWindow(loaded_itemos_list)
+        win.show_all()
+        self.disconnect_by_func(Gtk.main.quit)
+        self.close()
+    def launch_load(self): ## método para realizar hilos secundarios
+        thread = threading.Thread(target=self.load_json, args=())
+        thread.start()
+
+    def load_json(self):
+        response = requests.get('https://raw.githubusercontent.com/PabloDopico/Desarrollo-de-interfaces/main/catalog.json')
+        json_list = response.json()
+
+        result = [] ## inicializamos una lista para guardar los resultados
+
+        for json_item in json_list:  ## recorremos los elementos de la lista
+            name = json_item.get("name")  ## almacenamos en las 3 variables los valores del json
+            description = json_item.get("description")
+            image_url = json_item.get("image_url")
+            r = requests.get(image_url,stream=True)  ## descargamos la imagen
+            with open("temp.png",'wb') as f:
+                shutil.copyfileobj(r.raw, f)  ## convertimos la imagen en un archivo temporal
+            image = Gtk.Image.new_from_file("temp.png")  ## creamos un Gtk.Image para almacenar la imagen
+            result.append({"name": name, "description":description,"gtk_image":image})  ## añadimos los valores almacenados a un diccionario en la lista shutil
+        GLib.idle_add(self.start_main_window,result)
--- sprint2catalog/window.py
+++ sprint2catalog/window.py
@@ -8,7 +8,7 @@ from cell import Cell
 class MainWindow(Gtk.Window):  ## declaramos una ventana principal, cuya superclase es Gtk.Window
     flowbox = Gtk.FlowBox()  ## declaramos e inicializamos un componente FlowBox, para añadir elementos y reposicionarlos de acuerdo al tamaño disponible
 
-    def __init__(self):
+    def __init__(self,data_source):
         super().__init__(title="Catálogo")  ## titulo de la ventana
         self.connect("destroy", Gtk.main_quit)
         self.set_border_width(15)  ## declaramos la anchura del borde
@@ -26,35 +26,6 @@ class MainWindow(Gtk.Window):  ## declaramos una ventana principal, cuya supercl
         scrolled.add(self.flowbox)
         self.add(scrolled)
 
-        image = Gtk.Image()
-        ## con pixbuf asignamos la ruta y los pixeles a los que queremos reajustar la imagen
-        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/camaleon.png", 200, 200, False)
-        image.set_from_pixbuf(pixbuf)  ## asignamos los valores del pixbuf a la variable image
-
-        image2 = Gtk.Image()
-        pixbuf2 = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/conejo.png", 200, 200, False)
-        image2.set_from_pixbuf(pixbuf2)
-
-        image3 = Gtk.Image()
-        pixbuf3 = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/perro.png", 200, 200, False)
-        image3.set_from_pixbuf(pixbuf3)
-
-        image4 = Gtk.Image()
-        pixbuf4 = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/pinguino.png", 200, 200, False)
-        image4.set_from_pixbuf(pixbuf4)
-
-        image5 = Gtk.Image()
-        pixbuf5 = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/tiburon.png", 200, 200, False)
-        image5.set_from_pixbuf(pixbuf5)
-
-        cell_one = Cell("Camaleón", image)  ## indicamos los valores de la celda (el titulo y la imagen indicada mediante el pixbuf)
-        cell_two = Cell("Conejo", image2)
-        cell_three = Cell("Perro", image3)
-        cell_four = Cell("Pingüino", image4)
-        cell_five = Cell("Tiburón", image5)
-
-        self.flowbox.add(cell_one)  ## añadimos las celdas a la ventana
-        self.flowbox.add(cell_two)
-        self.flowbox.add(cell_three)
-        self.flowbox.add(cell_four)
-        self.flowbox.add(cell_five)
+        for item in data_source:
+            cell = Cell(item.get("name"), item.get("gtk_image"))
+            self.flowbox.add(cell)
