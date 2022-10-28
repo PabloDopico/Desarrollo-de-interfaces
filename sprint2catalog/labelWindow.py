de97fca6f09f2c049293f6a009fe4fb6ff30b5fb
Pablo Dopico <pdopicob@fpcoruna.afundacion.org>
2022-10-28 09:15:57 +0200

    Comentadas las líneas de código que faltaban

--- sprint2catalog/labelWindow.py
+++ sprint2catalog/labelWindow.py
@@ -7,9 +7,9 @@ from gi.repository import Gtk
 class labelWindow(Gtk.Window):
 
     def __init__(self):
-        super().__init__(title="acerca del desarrollador")
-        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        label = Gtk.Label("acerca del desarrollador")
+        super().__init__(title="acerca del desarrollador")  ## establecemos el titulo de la ventana
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)  ## creamos una caja normal con orientacion vertical
+        label = Gtk.Label("acerca del desarrollador")  ## creamos una label con la etiqueta necesaria para el ejercicio
         self.add(box)
         box.pack_start(label, True, True, 0)
 
--- sprint2catalog/window.py
+++ sprint2catalog/window.py
@@ -19,18 +19,19 @@ class MainWindow(Gtk.Window):  ## declaramos una ventana principal, cuya supercl
 
         ###################################################
 
-        menuBar = Gtk.MenuBar()
+        menuBar = Gtk.MenuBar()  ## creamos un menubar
 
-        filemenu = Gtk.Menu()
-        ayuda = Gtk.MenuItem("Ayuda")
-        ayuda.set_submenu(filemenu)
+        filemenu = Gtk.Menu()  ## creamos un menu
+        ayuda = Gtk.MenuItem("Ayuda")  ## creamos un item para el menu
+        ayuda.set_submenu(filemenu)  ## establecemos el item como submenu
 
-        acercaDe = Gtk.MenuItem("Acerca de")
-        acercaDe.connect("button-release-event", self.on_click)
-        vbox = Gtk.VBox()
-        vbox.pack_start(menuBar, False, False, 0)
-        self.add(vbox)
-        menuBar.append(ayuda)
+        acercaDe = Gtk.MenuItem("Acerca de")  ## creamos otro item
+        acercaDe.connect("button-release-event", self.on_click)  ## lo conectamos al click del ratón
+        vbox = Gtk.VBox()  ## creamos una vertical box (caja vertical)
+        vbox.pack_start(menuBar, False, False, 0)  ## añadimos el menú a la caja vertical
+        self.add(vbox)  ## añadimos la caja
+        menuBar.append(ayuda)  ## Añadimos el item al Menubar
+
         ###################################################
 
         header = Gtk.HeaderBar(title="Animales")  ## indicamos el titulo que aparecerá en la cabecera de la ventana
@@ -44,22 +45,17 @@ class MainWindow(Gtk.Window):  ## declaramos una ventana principal, cuya supercl
                             Gtk.PolicyType.AUTOMATIC)  ## indicamos políticas del scrolledwindow (solo en vertical y automatico)
         scrolled.add(self.flowbox)
 
-        self.add(vbox)
-        vbox.add(scrolled)
-        for item in data_source:
+        self.add(vbox)  ## añadimos la caja vertical
+        vbox.add(scrolled)  ## añadimos el scrolled a la caja virtual
+        for item in data_source:  ## recorremos todos los items en data_source
             cell = Cell(item.get("name"), item.get("gtk_image"),
                         item.get("description"))  ## añadimos a la celda los nombres, imagenes y descripciones acumulados
             self.flowbox.add(cell)  ## añadimos la celda
 
-        self.set_position(Gtk.WindowPosition.CENTER)
+        self.set_position(Gtk.WindowPosition.CENTER)  ## indicamos que la ventana debe estar centrada en pantalla
 
-        filemenu.append(acercaDe)
+        filemenu.append(acercaDe)  ## añadimos el item acercaDe al menu
 
-
-
-
-
-
-    def on_click(self, widget, event):
-        win = labelWindow()
+    def on_click(self, widget, event):  ## establecemos lo que hará el programa al clickar en el menú
+        win = labelWindow()  ## en este caso, mostrará una nueva ventana con la etiqueta
         win.show_all()
